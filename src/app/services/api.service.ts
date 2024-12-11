8c147e4e83047d8bf46b755342291fbf51ec1c68
Mario Castrillon <mario@hootfix.com>
2024-12-11 10:09:35 -0500

    last fixes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,6 +10,7 @@ const formHeaders = new HttpHeaders().set("rcy-key", "Jxa4lyCUktiBkhC0IiL4");
 })
 export class ApiService {
   endpoint = "https://api.ikosten.com/api/";
+  
 
   //endpoint = "http://localhost:4000/api/";
   //
